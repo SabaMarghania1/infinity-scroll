3153626eaf4737cbfec8726bc1aca7824c79e784
Saba Marghania <marghanias@gmail.com>
2024-02-07 09:29:39 +0400

    add:loading animation finish:Project

--- 01.quote-generator/script.js
+++ 01.quote-generator/script.js
@@ -3,12 +3,26 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
+
+// Show Spinner shown
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Remove Loaing Spinner
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
 
 // Show New Quote
 
 function newQuote() {
+  loading();
   // Pick a random quote from apiQuotes Array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   // Check if Author filed is blank and replace it with "unknown"
@@ -19,19 +33,21 @@ function newQuote() {
     authorText.textContent = quote.author;
   }
 
-  //Check if quote length to determine styling
+  //Check  quote length to determine styling
   if (quote.text.length > 80) {
     quoteText.classList.add("long-quote");
   } else {
     quoteText.classList.remove("long-quote");
   }
-  quoteText.textContent = quote.text;
 
-  //
+  //   Set quote, hide loader
+  quoteText.textContent = quote.text;
+  complete();
 }
 
 // Get Quotes From API
 async function getQuotes() {
+  loading();
   const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
   try {
     const response = await fetch(apiUrl);
@@ -43,6 +59,7 @@ async function getQuotes() {
 }
 
 // On Load
+
 getQuotes();
 
 // Tweet Quote
@@ -50,8 +67,6 @@ function tweetQuote() {
   const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
   window.open(twitterUrl, "_blank");
 }
-
-tweetQuote();
 
 // Event Listeners
 newQuoteBtn.addEventListener("click", newQuote);
